87671b1011d339fe2af842ce28c6093941164124
arifuzzaman arif <ac.arifarifuzzaman1@gmail.com>
2022-03-12 12:22:22 +0600

    first commit

---
+++ src/routes/userRoutes.tsx
@@ -0,0 +1,14 @@
+// const userRoutes = [
+//   {
+//     path: "/profile",
+//     name: "profile",
+//     component: <Profile />,
+//   },
+// ];
+import React from "react";
+
+const userRoutes = () => {
+  return <div>demo</div>;
+};
+
+export default userRoutes;
